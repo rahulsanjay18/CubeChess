21e33f7a018204c113cebc5425cf4365a09ff918
Rahul Shah <rahulsanjay18@gmail.com>
2021-05-07 21:42:40 -0400

    I feel like im doing moves wrong but whatever idk

--- piece.py
+++ piece.py
@@ -1,3 +1,14 @@
+class Piece:
+
+    def __init__():
+        self.sym = ''
+
+    def get_color(self):
+        return self.sym.upper() == self.sym
+    
+    def get_next_moves(self, coords):
+        raise NotImplementedError
+
 class PieceFactory:
     """"
     Represents a factory that creates a  piece superclass that each piece inherits from. Each piece has the following functions:
@@ -9,52 +20,163 @@ class PieceFactory:
 
     """
 
-    def create_piece(self, piece_name, x=-1, y=-1, z=-1, is_white=True):
+    def create_piece(self, piece_name, is_white=True):
+        
+
         if(piece_name.lower() == 'king' or piece_name.lower() == 'k'):
-            return King(x, y, z, is_white)
+            return King(is_white)
         elif(piece_name.lower() == 'queen' or piece_name.lower() == 'q'):
-            return Queen(x, y, z, is_white)
+            return Queen(is_white)
         elif(piece_name.lower() == 'bishop' or piece_name.lower() == 'b'):
-            return Bishop(x, y, z, is_white)
-        elif(piece_name.lower() == 'priest' or piece_name.lower() == 'p'):
-            return Priest(x, y, z, is_white)
+            return Bishop(is_white)
+        elif(piece_name.lower() == 'priest' or piece_name.lower() == 't'):
+            return Priest(is_white)
         elif(piece_name.lower() == 'knight' or piece_name.lower() == 'n'):
-            return Knight(x, y, z, is_white)
-        elif(piece_name.lower() == 'paladin' or piece_name.lower() == 'l'):
-            return Paladin(x, y, z, is_white)
+            return Knight(is_white)
+        elif(piece_name.lower() == 'paladin' or piece_name.lower() == 'p'):
+            return Paladin(is_white)
         elif(piece_name.lower() == 'rook' or piece_name.lower() == 'r'):
-            return Rook(x, y, z, is_white)
+            return Rook(is_white)
         elif(piece_name.lower() == 'pawn' or piece_name.lower() == 'i'):
-            return Pawn(x, y, z, is_white)
+            return Pawn(is_white)
         elif(piece_name.lower() == 'dragon' or piece_name.lower() == 'd'):
             return Dragon(x, y, z, is_white)
 
 
-class Rook:
+class Rook(Piece):
     def __init__(self, is_white=True):
-        self.piece_representation = 'R' if is_white else 'r'
-        self.is_white = is_white
-        
+        self.sym = 'R' if is_white else 'r'
 
-    def next_legal_move(self, x, y, z):
+    def get_next_moves(self, coords):
+        assert len(coords) == 3
+
         next_moves = []
 
         for i in range(8):
-            if(x == i):
+            if(coords[0] == i):
                 continue
-            next_moves.append(i, y, z)
+            next_moves.append((i, coords[1], coords[2]))
 
         for i in range(8):
-            if(y == i):
+            if(coords[1] == i):
                 continue
-            next_moves.append(x, i, z)
+            next_moves.append((coords[0], i, coords[2]))
 
         for i in range(8):
-            if(z == i):
+            if(coords[2] == i):
                 continue
-            next_moves.append(x, y, i)
+            next_moves.append((coords[0], coords[1], i))
 
         return next_moves
+
+class Bishop(Piece):
+    def __init__(self, is_white=True):
+        self.sym = 'B' if is_white else 'b'
+
+    def get_next_moves(self, coords):
+        assert len(coords) == 3
+
+        next_moves = []
+        
+        # i cant think of an algorithm that makes me happy, so im going to do
+        # an interesting one instead
+
+        adders = []
+
+        for i in range(8):
+            adders.append((i, i, i))
+            adders.append((-i, -i, -i))
+            
+            adders.append((-i, i, i))
+            adders.append((i, -i, -i))
+
+            adders.append((i, -i, i))
+            adders.append((-i, i, -i))
+           
+            adders.append((i, i, -i))
+            adders.append((-i, -i, i))
+
+
+        for adder in adders:
+            move = tuple(map(lambda i, j: i + j, adder, coords))
+
+            if(8 > move[0] >= 0 and 8 > move[1] >= 0 and 8 > move[2] >= 0):
+                next_moves.append(move)
+
+        return next_moves
+
+class Priest(Piece):
+    def __init__(is_white=True):
+        self.sym = 'T' if is_white else 't'
+
+    def get_next_moves(self, coords):
+        assert len(coords) == 3
+
+        next_moves= []
+        adders = []
+
+        for i in range(1, 8):
+            adders.append((0, i, i))
+            adders.append((i, 0, i))
+            adders.append((i, i, 0))
+
+            adders.append((0, -i, -i))
+            adders.append((-i, 0, -i))
+            adders.append((-i, -i, 0))
+            
+            adders.append((0, -i, i))
+            adders.append((-i, 0, i))
+            adders.append((-i, i, 0))
+
+            adders.append((0, i, -i))
+            adders.append((i, 0, -i))
+            adders.append((i, -i, 0))
+            
+
+
+        for adder in adders:
+            move = tuple(map(lambda i, j: i + j, adder, coords))
+
+            if(8 > move[0] >= 0 and 8 > move[1] >= 0 and 8 > move[2] >= 0):
+                next_moves.append(move)
+
+        return next_moves
+
+
+class Knight(Piece):
+    def __init__(is_white=True):
+        self.sym = 'K' if is_white else 'k'
+
+    def get_next_moves(self, coords):
+        assert len(coords) == 3
+
+        next_moves = []
+        adders = []
+
+        adders.append((2, 1, 1))
+        adders.append((-2, -1, -1))
+        
+        adders.append((2, 1, 1))
+        adders.append((-2, -1, -1))
+        
+        adders.append((2, 1, 1))
+        adders.append((-2, -1, -1))
+        
+        adders.append((2, 1, 1))
+        adders.append((-2, -1, -1))
+
+
+
+
+
+        for adder in adders:
+            move = tuple(map(lambda i, j: i + j, adder, coords))
+
+            if(8 > move[0] >= 0 and 8 > move[1] >= 0 and 8 > move[2] >= 0):
+                next_moves.append(move)
+
+
+
 
 
 
