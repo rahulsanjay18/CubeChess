e96fb4d93f0b4b45961cb103fb8a116be11310c7
Rahul Shah <rahulsanjay18@gmail.com>
2021-05-20 15:08:58 -0400

    added pawn implementation

--- piece.py
+++ piece.py
@@ -14,6 +14,9 @@ class Piece:
     def gen_path_step(self, coords, new_coords):
         raise NotImplementedError
 
+    def is_valid_attack(self, coords, new_coords):
+        return self.is_valid_move(self, coords, new_coords)
+
 class PieceFactory:
     """"
     Represents a factory that creates a  piece superclass that each piece inherits from. Each piece has the following functions:
@@ -348,15 +351,45 @@ class King(Piece):
                     abs(new_coords[1] - coords[1]),
                     abs(new_coords[2] - (coords[2]))
 
-        if sum(abs_diff) == 0:
+        if sum(abs_diff) == 1:
             return utils.check_if_valid_move(new_coords)
+
+        return False
 
     def gen_next_path(self, coords, new_coords):
         
         raise Exception("King does not have path")
 
 
+class Pawn(Piece):
 
+    def __init__(is_white=True):
+        self.sym = 'I' if is_white else 'i'
+
+    def is_valid_move(self, coords, new_coords):
+        diff = (new_coords[0] - coords[0],
+                new_coords[1] - coords[1],
+                new_coords[2] - coords[2])
+
+        if diff[0] >= 0 and diff[1] >= 0 and diff[2] >= 0 and sum(diff) == 1:
+            return utils.check_if_valid_move(new_coords)
+        
+        return False
+    
+    def gen_next_path(self, coords, new_coords):
+        return Exception("Pawn moves one step, we don't need to check the
+            path")
+
+    def is_valid_attack(self, coords, new_coords):
+        diff = (new_coords[0] - coords[0],
+                new_coords[1] - coords[1],
+                new_coords[2] - coords[2])
+        
+        if (diff[0] == 0 or diff[1] == 0 or diff[2] == 0) and sum(diff) == 2:
+            if (diff[0] == 1 or diff[1] == 1 or diff[2] == 1):
+                return utils.check_if_valid_move(new_coords)
+
+        return False
 
         
 
