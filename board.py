da9ca38ee5b419b33d9d264a7a090427b373a04c
Rahul Shah <rahulsanjay18@gmail.com>
2021-06-04 22:15:22 -0400

    added white pieces

--- board.py
+++ board.py
@@ -7,13 +7,13 @@ class Board:
         '''
 
 	def __init__(self):
-            self.board = [[['_' for k in range(8)] for j in range(8)] for i in
+            self.board = [[[None for k in range(8)] for j in range(8)] for i in
                     range(8)]
        
         def get(self, coords):
             return self.board[coords[0]][coords[1]][coords[2]]
 
-        def insert(self, coords, piece='_'):
+        def insert(self, coords, piece = None):
             self.board[coords[0]][coords[1]][coords[2]] = piece
 
 
--- game.py
+++ game.py
@@ -1,18 +1,70 @@
-import board
-import piece
+import board as b
+import piece as p
 
 class Game:
     
     is_white_turn = True
 
     def __init__():
-        self.board = Board()
+        self.board = b.Board()
     
     '''
     Initializes the board with the starting pieces
     '''
-    def initialize():
-        pass
+    def initialize(self):
+        king_w = p.King()
+        king_b = p.King(False)
+
+        queen_w = p.Queen()
+        queen_b = p.Queen(False)
+
+        bishop_w1 = p.Bishop()
+        bishop_w2 = p.Bishop()
+        bishop_b1 = p.Bishop(False)
+        bishop_b2 = p.Bishop(False)
+
+        priest_w1 = p.Priest()
+        priest_w2 = p.Priest()
+        priest_b1 = p.Priest(False)
+        priest_b2 = p.Priest(False)
+
+        paladin_w1 = p.Paladin()
+        paladin_w2 = p.Paladin()
+        paladin_b1 = p.Paladin(False)
+        paladin_b2 = p.Paladin(False)
+
+        rook_w1 = p.Rook()
+        rook_w2 = p.Rook()
+        rook_b1 = p.Rook(False)
+        rook_b2 = p.Rook(False)
+
+        pawns_w = []
+        pawns_b = []
+
+        for i in range(18):
+            pawns_w.append(p.Pawn())
+
+        for i in range(18):
+            pawns_b.append(p.Pawn(False))
+
+        self.board.insert((7, 0, 0), king_w)
+        self.board.insert((6, 0, 0), king_w)
+        self.board.insert((5, 0, 0), king_w)
+        self.board.insert((4, 0, 0), king_w)
+        self.board.insert((3, 0, 0), king_w)
+        self.board.insert((2, 0, 0), king_w)
+        self.board.insert((1, 0, 0), king_w)
+        self.board.insert((0, 0, 0), king_w)
+        
+        for i in range(8):
+            self.board.insert((i, 0, 1), pawns_w[i])
+
+        for i in range(8):
+            self.board.insert((i, 1, 0), pawns_w[i+8])
+
+        self.board.insert((3, 1, 1), pawns_w[16])
+        self.board.insert((4, 1, 1), pawns_w[17])
+
 
     '''
     This is done in a few steps:
