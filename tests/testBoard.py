948db8fb162c8a632e17dd784b3074f0a6c2c5e8
Rahul Shah <rshah351@gatech.edu>
2022-12-17 15:11:44 -0500

    wrote test case outline

--- tests/testBoard.py
+++ tests/testBoard.py
@@ -10,9 +10,24 @@ class testBoard(unittest.TestCase):
     def testLayout(self):
         true_board = 'rd00r0dr/iiiiiiii/00000000/00000000/00000000/00000000/IIIIIIII/RD00R0DR\ndbpnnpbd/iiiiiiii/00000000/00000000/00000000/00000000/IIIIIIII/DBPNNPBD\n0pbttbp0/iiiiiiii/00000000/00000000/00000000/00000000/IIIIIIII/0PBTTBP0\nrntkqtn0/iiiiiiii/00000000/00000000/00000000/00000000/IIIIIIII/RNTKQTN0\n0ntqqtnr/iiiiiiii/00000000/00000000/00000000/00000000/IIIIIIII/0NTQQTNR\n0pbttbp0/iiiiiiii/00000000/00000000/00000000/00000000/IIIIIIII/0PBTTBP0\ndbpnnpbd/iiiiiiii/00000000/00000000/00000000/00000000/IIIIIIII/DBPNNPBD\nrd0r00dr/iiiiiiii/00000000/00000000/00000000/00000000/IIIIIIII/RD0R00DR'
         
-        self.assertEquals(true_board, str(self.test_game.board))
-        
+        self.assertEqual(true_board, str(self.test_game.board))
 
-
+    '''
+    makes a move and tests if the board reflects that change
+    ''' 
+    def testBoardMovement(self):
+        pass
+    
+    '''
+    tests insert function and see if board makes that change
+    '''
+    def test_insert(self):
+        pass
+    
+    '''
+    get piece from board, make sure it is the right piece
+    '''
+    def test_get(self):
+        pass
 if __name__ == '__main__':
     unittest.main()
---
+++ tests/testGame.py
@@ -0,0 +1,51 @@
+import unittest
+from game import Game
+import random
+
+class testGame(unittest.TestCase):
+    def setUp(self) -> None:
+        self.test_game = Game()
+        return super().setUp()
+    
+    '''
+    test make_move (happy case), move pawn, then another piece.
+    Check result.
+    '''
+    def test_make_move(self):
+        pass
+
+    '''
+    test make_move (invalid position at start)
+    '''
+    def test_invalid_start(self):
+        pass
+
+    '''
+    test make_move (invalid position at end)
+    '''
+    def test_invalid_end(self):
+        pass
+
+    '''
+    test make_move (move coordinates valid but no piece in space)
+    '''
+    def test_no_piece(self):
+        pass
+
+    '''
+    test make_move (move piece to capture piece of same color)
+    '''
+    def test_same_color(self):
+        pass
+
+    '''
+    test make_move (piece move is invalid). We don't need to test all pieces, just one piece.
+    '''
+    def test_piece_move_invalid(self):
+        pass
+
+    '''
+    test make_move (block path for a piece that needs a clear path)
+    '''
+    def test_blocked_movement(self):
+        pass
